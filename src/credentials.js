383381acc4d334a6b3039fa3a2ceadcc44f2a9f3
Mai Huy Bảo <justonly.maihuybao@gmail.com>
2021-02-10 11:52:10 +0000

    Create credentials.js

---
+++ src/credentials.js
@@ -0,0 +1,16 @@
+// Facebook log-in credentials for bot account
+exports.FACEBOOK_EMAIL = "";
+exports.FACEBOOK_PASSWORD = "";
+// Heroku application token
+exports.TOKEN = "";
+// MemCachier credentials (from MemCachier dashboard)
+exports.MEMCACHIER_PASSWORD = "";
+exports.MEMCACHIER_SERVERS = "";
+exports.MEMCACHIER_USERNAME = "";
+// Spotify API credentials
+exports.SPOTIFY_CLIENTID = "";
+exports.SPOTIFY_CLIENTSECRET = "";
+// Wolfram short-answer API key
+exports.WOLFRAM_KEY = "";
+// Open Weather Map API key
+exports.WEATHER_KEY = "";
